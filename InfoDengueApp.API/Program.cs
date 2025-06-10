22d871f44269eef804bef68a3a6acd73c833a2e0
Lucas Pereira da Silva <lucassilva@fiotec.fiocruz.br>
2025-06-10 20:08:25 -0300

    feat(api): configuração do appsettings e documentação da api.

--- InfoDengueApp.API/Program.cs
+++ InfoDengueApp.API/Program.cs
@@ -1,21 +1,52 @@
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+
+//builder.Services.AddControllers();
+//// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+//builder.Services.AddOpenApi();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//}
+
+//app.UseAuthorization();
+
+//app.MapControllers();
+
+//app.Run();
+
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+//Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
+//Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//Scalar
+app.MapScalarApiReference(options => {
+    options.WithTheme(ScalarTheme.BluePlanet);
+});
+
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
